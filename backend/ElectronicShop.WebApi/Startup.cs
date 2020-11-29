7df5c6ae82bfdd842369a146afd7f1e9de77a494
Hieu Nguyen <nguyentrunghieu25051999@gmail.com>
2020-11-29 21:41:11 +0700

    add necessary services

--- backend/ElectronicShop.WebApi/Startup.cs
+++ backend/ElectronicShop.WebApi/Startup.cs
@@ -3,12 +3,19 @@ using ElectronicShop.Data.EF;
 using ElectronicShop.Data.Entities;
 using ElectronicShop.Services.SendMail;
 using ElectronicShop.Utilities.Mapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Text;
 
 namespace ElectronicShop.WebApi
 {
@@ -24,14 +31,83 @@ namespace ElectronicShop.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Inject AutoMapper
+            // Compatibility version for ASP.NET Core MVC
+            //services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            // For use Session
+            services.AddDistributedMemoryCache();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromHours(3);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            // In the accepted answer "Bearer " is required to be written before the actual token. 
+            // A similar approach in which typing "Bearer " can be skipped is the following:
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "ElectronicShop",
+                    Version = "v1"
+                });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please insert JWT with Bearer into field",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement {
+                   {
+                     new OpenApiSecurityScheme
+                     {
+                       Reference = new OpenApiReference
+                       {
+                         Type = ReferenceType.SecurityScheme,
+                         Id = "Bearer"
+                       }
+                      },
+                      new string[] { }
+                    }
+                  });
+            });
+
+            // Configure the authentication schema with JWT bearer options
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+           // Adding Jwt Bearer  
+           .AddJwtBearer(options =>
+           {
+               options.SaveToken = true;
+               options.RequireHttpsMetadata = false;
+               options.TokenValidationParameters = new TokenValidationParameters()
+               {
+                   ValidateIssuer = true,
+                   ValidateAudience = true,
+                   ValidAudience = Configuration["JWT:ValidAudience"],
+                   ValidIssuer = Configuration["JWT:ValidIssuer"],
+                   IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+               };
+           });
+
+            // For use AutoMapper
             services.AddAutoMapper(typeof(ElectronicShopProfile));
 
-            // Inject ElectronicShopDbContext
+            // For use ElectronicShopDbContext
             services.AddDbContext<ElectronicShopDbContext>();
             services.AddIdentity<User, Role>()
                 .AddEntityFrameworkStores<ElectronicShopDbContext>()
                 .AddDefaultTokenProviders();
+
+            // For .NET Core MVC Page Not Refreshing After Changes
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
             // Insert SMTP settings parser and initialize a singleton object that will handle mail service
             services.Configure<SmtpSettings>(Configuration.GetSection("SmtpSettings"));
@@ -48,11 +124,25 @@ namespace ElectronicShop.WebApi
                 app.UseDeveloperExceptionPage();
             }
 
+
+
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ElectronicShop API V1");
+            });
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
