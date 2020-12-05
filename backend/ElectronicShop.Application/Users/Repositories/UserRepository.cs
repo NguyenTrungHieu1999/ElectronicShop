e1f33b51a24bda5750e4280b719ae3b9d411b4ed
Hieu Nguyen <nguyentrunghieu25051999@gmail.com>
2020-12-05 15:31:22 +0700

    update UserService

---
+++ backend/ElectronicShop.Application/Users/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using ElectronicShop.Application.Common.Repositories;
+using ElectronicShop.Data.EF;
+using ElectronicShop.Data.Entities;
+
+namespace ElectronicShop.Application.Users.Repositories
+{
+    public class UserRepository : RepositoryBase<User>, IUserRepository
+    {
+        public UserRepository(ElectronicShopDbContext context) : base(context)
+        {
+        }
+    }
+}
--- backend/ElectronicShop.Utilities/SystemConstants/Constants.cs
+++ backend/ElectronicShop.Utilities/SystemConstants/Constants.cs
@@ -3,5 +3,13 @@
     public class Constants
     {
         public const string CURRENTUSER = "CURRENTUSER";
+
+        public const string USERROLENAME = "USER";
+
+        public const string ADMINROLENAME = "ADMIN";
+
+        public const string ADMIN = "Admin";
+
+        public const string USER = "User";
     }
 }
