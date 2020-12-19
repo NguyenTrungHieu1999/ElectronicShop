0b686c802584bd3e78145c163c018aa821d5c814
Hieu Nguyen <nguyentrunghieu25051999@gmail.com>
2020-12-19 21:48:24 +0700

    create product with multiple images

---
+++ backend/ElectronicShop.Application/Authentications/Commands/SignOut/SignOutCommand.cs
@@ -0,0 +1,28 @@
+﻿//using ElectronicShop.Application.Authentications.Services;
+//using MediatR;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+//namespace ElectronicShop.Application.Authentications.Commands.SignOut
+//{
+//    public class SignOutCommand : IRequest<Unit>
+//    {
+
+//    }
+
+//    public class SignOutHandle : IRequestHandler<SignOutCommand>
+//    {
+//        private readonly IAuthService _authService;
+
+//        public SignOutHandle(IAuthService authService)
+//        {
+//            _authService = authService;
+//        }
+
+//        public async Task Handle(SignOutCommand request, CancellationToken cancellationToken)
+//        {
+//            await _authService.SignOutAsync();
+
+//        }
+//    }
+//}
