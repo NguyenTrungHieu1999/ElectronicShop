65a9d1702481b2899e07b98d22d86aa705a4bfd2
Hieu Nguyen <nguyentrunghieu25051999@gmail.com>
2020-12-31 13:41:56 +0700

    .

--- backend/ElectronicShop.Application/Users/Extensions/UserExtensions.cs
+++ backend/ElectronicShop.Application/Users/Extensions/UserExtensions.cs
@@ -9,12 +9,25 @@ namespace ElectronicShop.Application.Users.Extensions
         public static void Map(this User user, UpdateUserCommand request)
         {
             user.FirstMiddleName = request.FirstMiddleName;
+
             user.LastName = request.LastName;
+
             user.PhoneNumber = request.PhoneNumber;
-            user.Gender = request.Gender;
-            user.Birthday = request.Birthday;
+
+            if(request.Gender != null)
+            {
+                user.Gender = request.Gender;
+            }
+
+            if (request.Birthday != null)
+            {
+                user.Birthday = request.Birthday.GetValueOrDefault();
+            }
+
             user.Address = request.Address;
+
             user.Status = request.Status;
+
             user.ModifiedDate = DateTime.Now;
         }
     }
