b58c3b406d0493665e574c14a3ce1d3e15d74a07
Hieu Nguyen <nguyentrunghieu25051999@gmail.com>
2021-06-23 15:49:19 +0700

    upgrade to send email when ordering

---
+++ backend/ElectronicShop.Application/Orders/Models/OrderDetailForMail.cs
@@ -0,0 +1,10 @@
+﻿namespace ElectronicShop.Application.Orders.Models
+{
+    public class OrderDetailForMail
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
--- backend/ElectronicShop.WebApi/Controllers/OrdersController.cs
+++ backend/ElectronicShop.WebApi/Controllers/OrdersController.cs
@@ -1,4 +1,7 @@
-﻿using ElectronicShop.Application.Orders.Commands.ChangeOrderStatus;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using ElectronicShop.Application.Orders.Commands.ChangeOrderStatus;
 using ElectronicShop.Application.Orders.Commands.CreateOrder;
 using ElectronicShop.Application.Orders.Queries.GetAllOrder;
 using ElectronicShop.Application.Orders.Queries.GetOrderByUserId;
@@ -11,9 +14,13 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using ElectronicShop.Application.Orders.Commands.CancleMyOrder;
 using ElectronicShop.Application.Orders.Commands.CancleOrder;
+using ElectronicShop.Application.Orders.Models;
 using ElectronicShop.Application.Orders.Queries.GetOrderById;
 using ElectronicShop.Application.Orders.Queries.HaveOrder;
 using ElectronicShop.Application.Orders.Queries.MyOrderById;
+using ElectronicShop.Data.EF;
+using ElectronicShop.Infrastructure.SendMail;
+using Microsoft.AspNetCore.Http;
 
 namespace ElectronicShop.WebApi.Controllers
 {
@@ -22,10 +29,16 @@ namespace ElectronicShop.WebApi.Controllers
     public class OrdersController : BaseController
     {
         private readonly IMediator _mediator;
+        private readonly ElectronicShopDbContext _context;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IMailer _mailer;
 
-        public OrdersController(IMediator mediator)
+        public OrdersController(IMediator mediator, ElectronicShopDbContext context, IHttpContextAccessor httpContextAccessor, IMailer mailer)
         {
             _mediator = mediator;
+            _context = context;
+            _httpContextAccessor = httpContextAccessor;
+            _mailer = mailer;
         }
         
         [HttpPost("create")]
@@ -33,7 +46,32 @@ namespace ElectronicShop.WebApi.Controllers
         [AllowAnonymous]
         public async Task<IActionResult> Create(CreateOrderCommand command)
         {
-            return Ok(await _mediator.Send(command));
+            var result = await _mediator.Send(command);
+
+            if (result.IsSuccessed)
+            {
+                var orderDetails = new List<OrderDetailForMail>();
+                foreach (var order in command.OrderDetails)
+                {
+                    var product = await _context.Products.FindAsync(order.ProductId);
+                    orderDetails.Add(new OrderDetailForMail
+                    {
+                        Id = order.ProductId,
+                        Price = order.Price,
+                        ProductName = product.Name,
+                        Quantity = order.Quantity
+                    });
+                }
+                string body = "<h3>Cảm ơn quý khách " + command.Receiver + " đã đặt hàng tại ElectronicShop,</h3><p>Đơn hàng sẽ được giao đến địa chỉ của quý khách trong thời gian sớm nhất, ElectronicShop sẽ thông báo đến quý khách ngay khi hàng chuẩn bị được giao.</p>" + "<h3>Thông tin đơn hàng của quý khách bao gồm:</h3>"
+                              + "<p><strong> Người nhận:</strong> " + command.Receiver + "</p>"
+                              + "<p><strong> Địa chỉ:</strong> " + command.ReceiversAddress + "</p>"
+                              + "<p><strong> Số điện thoại:</strong> " + command.PhoneNumber + "</p>"
+                              + "<div style=\"position: absolute;\">" + "<table border=\"1px\" style=\"border-spacing: 0\"><tr><th>Mã Sản phẩm</th><th>Tên sản phẩm</th><th>Số lượng</th><th>Đơn giá</th></tr>";
+                body = orderDetails.Aggregate(body, (current, item) => current + ("<tr><th><p>" + item.Id + "</p></th>" + "<th><p>" + item.ProductName + "</p></th>" + "<th><p>" + item.Quantity + "</p></th>" + "<th><p>" + item.Price.ToString("N0") + " VND </p></th></tr>"));
+                body += "</table><p style=\"padding-right:10px\" >Tổng tiền: <span style=\"color: red\">" + command.TotalMoney.ToString("N0") + " VND</span></p><h4>Một lần nữa ElectronicShop cảm ơn quý khách!!!</h4></div>";
+                await _mailer.SenEmailAsync("hieutanmy321@gmail.com", "Thông tin đặt hàng", body);
+            }
+            return Ok(result);
         }
 
         [HttpPost("{orderId}/change-status")]
