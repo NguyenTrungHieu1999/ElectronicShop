b1a8c69a18c7cdd0dcd69cb06d49a51b43757928
Hieu Nguyen <nguyentrunghieu25051999@gmail.com>
2021-07-18 13:28:51 +0700

    enable account

---
+++ backend/ElectronicShop.Application/Users/Commands/EnableAccount/EnableAccountCommand.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using ElectronicShop.Application.Common.Models;
+using ElectronicShop.Application.Users.Services;
+using MediatR;
+
+namespace ElectronicShop.Application.Users.Commands.EnableAccount
+{
+    public class EnableAccountCommand : IRequest<ApiResult<string>>
+    {
+        public EnableAccountCommand(int userId)
+        {
+            UserId = userId;
+        }
+
+        public int UserId { get; }
+    }
+    
+    public class EnableAccountHandle : IRequestHandler<EnableAccountCommand, ApiResult<string>>
+    {
+        private readonly IUserService _service;
+
+        public EnableAccountHandle(IUserService service)
+        {
+            _service = service;
+        }
+
+        public async Task<ApiResult<string>> Handle(EnableAccountCommand request, CancellationToken cancellationToken)
+        {
+            return await _service.EnableAccountAsync(request.UserId);
+        }
+    }
+}
